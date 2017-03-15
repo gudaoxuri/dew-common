1fa6f8e5c625f92242ec5cf4e6fd405857caa94c
gudaoxuri <i@sunisle.org>
2017-03-15 10:16:05 +0800

    Add error convert

--- src/main/java/com/ecfront/dew/common/JsonHelper.java
+++ src/main/java/com/ecfront/dew/common/JsonHelper.java
@@ -30,44 +30,60 @@ public class JsonHelper {
         MAPPER.setTimeZone(tz);
     }
 
-    public static String toJsonString(Object obj) throws JsonProcessingException {
+    public static String toJsonString(Object obj) throws RuntimeException {
         if (obj instanceof String) {
             return (String) obj;
         } else {
-            return MAPPER.writeValueAsString(obj);
+            try {
+                return MAPPER.writeValueAsString(obj);
+            } catch (JsonProcessingException e) {
+                throw new RuntimeException(e);
+            }
         }
     }
 
-    public static JsonNode toJson(Object obj) throws IOException {
+    public static JsonNode toJson(Object obj) throws RuntimeException {
         if (obj instanceof String) {
-            return MAPPER.readTree((String) obj);
+            try {
+                return MAPPER.readTree((String) obj);
+            } catch (IOException e) {
+                throw new RuntimeException(e);
+            }
         } else {
             return MAPPER.valueToTree(obj);
         }
     }
 
-    public static <E> List<E> toList(Object obj, Class<E> clazz) throws IOException {
+    public static <E> List<E> toList(Object obj, Class<E> clazz) throws RuntimeException {
         JavaType type = MAPPER.getTypeFactory().constructParametricType(List.class, clazz);
-        if (obj instanceof String) {
-            return MAPPER.readValue((String) obj, type);
-        } else if (obj instanceof JsonNode) {
-            return MAPPER.readValue(obj.toString(), type);
-        } else {
-            return MAPPER.readValue(MAPPER.writeValueAsString(obj), type);
+        try {
+            if (obj instanceof String) {
+                return MAPPER.readValue((String) obj, type);
+            } else if (obj instanceof JsonNode) {
+                return MAPPER.readValue(obj.toString(), type);
+            } else {
+                return MAPPER.readValue(MAPPER.writeValueAsString(obj), type);
+            }
+        } catch (IOException e) {
+            throw new RuntimeException(e);
         }
     }
 
-    public static <E> E toObject(Object obj, Class<E> clazz) throws IOException {
-        if (obj instanceof String) {
-            if (clazz == String.class) {
-                return (E) obj;
+    public static <E> E toObject(Object obj, Class<E> clazz) throws RuntimeException {
+        try {
+            if (obj instanceof String) {
+                if (clazz == String.class) {
+                    return (E) obj;
+                } else {
+                    return MAPPER.readValue((String) obj, clazz);
+                }
+            } else if (obj instanceof JsonNode) {
+                return MAPPER.readValue(obj.toString(), clazz);
             } else {
-                return MAPPER.readValue((String) obj, clazz);
+                return MAPPER.readValue(MAPPER.writeValueAsString(obj), clazz);
             }
-        } else if (obj instanceof JsonNode) {
-            return MAPPER.readValue(obj.toString(), clazz);
-        } else {
-            return MAPPER.readValue(MAPPER.writeValueAsString(obj), clazz);
+        } catch (IOException e) {
+            throw new RuntimeException(e);
         }
     }
 
